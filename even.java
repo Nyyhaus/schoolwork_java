d75d2365a3c55b0359b8cf2460cceceb1ce2b0dd
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-17 12:50:09 +0200

    Create even.java

---
+++ even.java
@@ -0,0 +1,19 @@
+public class Parillisuus {
+	public static void main(String[] args) {
+		int luku;
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			System.out.print("Anna kokonaisluku: ");
+			luku = Integer.parseInt(in.readLine());
+			
+			if (luku % 2 == 0)
+				System.out.println("Luku " + luku + " on parillinen.");
+			else
+				System.out.println("Luku " + luku + " on pariton.");
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+	}
+}
